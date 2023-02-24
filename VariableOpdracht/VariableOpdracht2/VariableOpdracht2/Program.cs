fc6bc51c1c9ebf4af2b4590165b416834ae708cd
Safwan <35345@ma-web.nl>
2023-02-24 10:36:33 +0100

    variableas

---
+++ VariableOpdracht/VariableOpdracht2/VariableOpdracht2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class Program
+    {
+        //class variables & gebruik
+        static void Main(string[] args)
+        {
+
+            //ga naar de file TwitchStream.cs in je solution explorer in visual studio
+            TwitchStream twitchStream = new TwitchStream();
+
+
+            //name, waarde: je favoriete streamer
+            //lastStreamName, waarde: de streamname van de laatste keer
+            //streamGame, waarde een nieuwe instance van Game (new Game("naam van de game hier"})
+            //followers, waarde: hoeveel followers
+            //automod, waarde: staat automod aan?
+            twitchStream.name = "Pyrocynical";
+            twitchStream.LastStreamName = "Wales is SHIT";
+            Game streamGame = new Game("Blooodborne");
+           
+            twitchStream.followers = 2550;
+            twitchStream.automod = true;
+           
+
+
+
+            Console.WriteLine("his new game is "+ streamGame.name);
+
+        }
+    }
+}
---
+++ VariableOpdracht/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class TwitchStream
+    {
+        internal string name;
+        internal string LastStreamName;
+        internal string StreamGame;
+        internal int followers;
+        internal bool automod ;
+
+    }
+}
