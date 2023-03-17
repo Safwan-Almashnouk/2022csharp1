2e1644016dd0bb2952d5e004fe2b01cb0544c3ce
Safwan <35345@ma-web.nl>
2023-03-17 12:06:11 +0100

    Bah

---
+++ ArrayOpdracht/ArrayOpdracht4/ArrayOpdracht4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArrayOpdracht4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Enemy [] enemies = new Enemy[10] ;
+
+
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                enemies[i] = new Enemy(i);
+            }
+
+        }
+    
+    }
+
+}
