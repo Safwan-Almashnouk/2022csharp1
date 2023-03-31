b13887a44e1e64c8859f8b1ae1afdc0a4536f3f5
Safwan <35345@ma-web.nl>
2023-03-31 10:26:18 +0200

    a

---
+++ ques/PropertyOpdracht1/PropertyOpdracht1/Persoon.cs
@@ -0,0 +1,14 @@
+﻿namespace PropertyOpdracht1
+{
+    internal class Persoon
+    {
+	//hier komen je properties
+	
+        public Persoon()
+        {
+        }
+        public string Naam { get; internal set; }
+        public int Leeftijd { get; internal set; }
+
+    }
+}
---
+++ ques/PropertyOpdracht3/PropertyOpdracht3/Mens.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Transactions;
+using System.Windows.Markup;
+
+namespace PropertyOpdracht3
+{
+    internal class Mens
+    {
+        private int leeftijd;
+
+        //maak hier een property Leeftijd:
+        public int Leeftijd
+        {
+            get
+            {
+                return leeftijd;
+            }
+            set
+            {
+                if (value > leeftijd)
+                {
+                    leeftijd = value;
+                    Console.WriteLine("no problemo mi amigos ");
+
+                }
+                else
+                {
+                    Console.WriteLine("no");
+                }
+            }
+        }
+		
+		//get: returned leeftijd
+		//set:
+		// deze moet controlleren of de nieuwe leeftijd wel hoger (>) is dan de huidige
+		// als deze hoger is dan zet je de waarde op leeftijd
+		//als lager, dan Console.WriteLine("dat kan niet!");
+
+        public Mens(int leeftijd)
+        {
+            this.leeftijd = leeftijd;
+        }
+    }
+}
